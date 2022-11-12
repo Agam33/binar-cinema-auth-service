8a720746c46cd7e5c6010973cc43b8039225d1d2
Riswan Agam <riswanagam@gmail.com>
2022-11-12 23:15:52 +0700

    #update

--- src/main/java/com/ra/bioskop/authservice/security/SecurityConfig.java
+++ src/main/java/com/ra/bioskop/authservice/security/SecurityConfig.java
@@ -61,13 +61,13 @@ public class SecurityConfig {
                 .permitAll()
 
                 // Admin
-                .antMatchers("/api/v1/films/addAll", "/api/v1/films/add", "/api/v1/films/delete",
-                        "/api/v1/films/addSchedule", "/api/v1/films/update", Constants.NOTIFICATION_ENDPOINT+"/**")
-                .hasRole("ADMIN")
+                // .antMatchers("/api/v1/films/addAll", "/api/v1/films/add", "/api/v1/films/delete",
+                //         "/api/v1/films/addSchedule", "/api/v1/films/update", Constants.NOTIFICATION_ENDPOINT+"/**")
+                // .hasRole("ADMIN")
 
-                // Customer
-                .antMatchers("/api/v1/user/update", "/api/v1/user/delete", "/api/invoice/download")
-                .hasRole("CUSTOMER")
+                // // Customer
+                // .antMatchers("/api/v1/user/update", "/api/v1/user/delete", "/api/invoice/download")
+                // .hasRole("CUSTOMER")
 
                 .anyRequest().authenticated()
 
